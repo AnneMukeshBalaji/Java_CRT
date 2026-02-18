fc1a1f7e3ebfc23d532977a6194d3766d3080325
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-18 11:33:53 +0530

    added another file

---
+++ Day1/DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+  public static void main(String[] args) {
+    int age = 21;
+    double height = 5.6;
+    char grade = 'A';
+    boolean passed = true;
+    System.out.printf("Age: %d\nHeight: %.1f\nBoolean: %b", age, height, passed);
+  }
+}
--- Day1/ScannerInput.java
+++ Day1/ScannerInput.java
@@ -16,3 +16,16 @@ public class ScannerInput {
     sc.close();
   }
 }
+/*
+ * NOTE: 1 byte = 8 bits
+ * NOTE: 1 Kilo = 2^10
+ * NOTE: 1 Mega = 2^20
+ * NOTE: 1 Giga = 2^30
+ * NOTE: 1 Tera = 2 ^40
+ * NOTE: 1 TB = 2^10 * 1GB
+ * ======================> 2 ^40 Bytes
+ * NOTE: 1 GB = 2^30 * 8 bits
+ * 
+ * TODO :
+ * 1] Bypte means 8 bits ... MSB is Sign and next 7 bits for Magnitude
+ */
