a51470833982d3a9f87280bee8a4e7640d92a3a1
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-19 14:23:14 +0530

    changed pattern5 file

--- Day2/Pattern5.java
+++ Day2/Pattern5.java
@@ -3,8 +3,14 @@ package Day2;
 public class Pattern5 {
   public static void main(String[] args) {
     int count = 0;
-    for (int i = 1; i <= 5; i++) {
-      for (int j = 1; j <= i; j++) {
+    int i, j;
+    for (i = 1; i <= 5; i++) {
+      if (i % 2 == 0) {
+        count = 0;
+      } else {
+        count = 1;
+      }
+      for (j = 1; j <= i; j++) {
         if (count % 2 == 0) {
           System.out.print("0 ");
         } else {
