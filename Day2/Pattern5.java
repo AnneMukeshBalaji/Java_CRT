8f1e18c4cf4f2617f8a4718889bcd38069181501
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-19 12:33:47 +0530

    added pattern5 file

---
+++ Day2/Pattern5.java
@@ -0,0 +1,31 @@
+package Day2;
+
+public class Pattern5 {
+  public static void main(String[] args) {
+    int count = 0;
+    for (int i = 1; i <= 5; i++) {
+      for (int j = 1; j <= i; j++) {
+        if (count % 2 == 0) {
+          System.out.print("0 ");
+        } else {
+          System.out.print("1 ");
+        }
+        count += 1;
+      }
+      System.out.println();
+    }
+  }
+}
+/*
+ *
+ * 0
+ * 1 0
+ * 1 0 1
+ * 0 1 0 1
+ * 0 1 0 1 0
+ *
+ *
+ *
+ * 
+ * 
+ */
