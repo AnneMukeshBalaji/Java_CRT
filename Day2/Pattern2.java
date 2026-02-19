ae2c72823dad3b9a32c023a40e90467c1deabffb
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-19 11:25:57 +0530

    added pattern2 file

--- Day2/Pattern2.java
+++ Day2/Pattern2.java
@@ -1,10 +1,10 @@
 package Day2;
 
-public class Pattern1 {
+public class Pattern2 {
   public static void main(String[] args) {
     for (int i = 1; i <= 5; i++) {
       for (int j = 1; j <= i; j++) {
-        System.out.print("* ");
+        System.out.printf("%d ", i);
       }
       System.out.println();
     }
