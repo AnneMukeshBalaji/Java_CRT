7d81c64a1668c8da716ea340f302dde965c52ee5
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-23 15:14:55 +0530

    added defaultcontructor file

---
+++ Day5/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package Day5;
+
+public class DefaultConstructor {
+  String name;
+  int age;
+
+  public static void main(String[] args) {
+    DefaultConstructor s = new DefaultConstructor();
+    System.out.println(s.name);
+    System.out.println(s.age);
+  }
+}
