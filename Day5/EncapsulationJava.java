fa2bfbfa51d1440dfe4a1689854e5b2bc8e39704
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-23 10:35:54 +0530

    modified some lines

--- Day5/EncapsulationJava.java
+++ Day5/EncapsulationJava.java
@@ -3,12 +3,6 @@ package Day5;
 public class EncapsulationJava {
   private int data;
 
-  public static void main(String[] args) {
-    // NOTE: Encapsulation Means it Encapuslates the Date the private data cannot be
-    // asscessed from other class
-
-  }
-
   public void setData(int data) {
     this.data = data;
   }
@@ -17,3 +11,9 @@ public class EncapsulationJava {
     return this.data;
   }
 }
+
+// NOTE: Encapsulation Means it Encapuslates the Date the private data cannot be
+// asscessed from other class
+// NOTE: To asscesse the priavte Data you can use get and Set methods they only
+// assign the copy value
+//
