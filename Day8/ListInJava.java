f8057abc4eb8e81eba0a997cef2dd6d94fcdc4a5
AnneMukeshBalaji <mukimukku@gmail.com>
2026-02-26 06:08:22 +0530

    initial commit of Day8

---
+++ Day8/ListInJava.java
@@ -0,0 +1,16 @@
+package Day8;
+
+import java.util.ArrayList;
+
+public class ListInJava {
+  public static void main(String[] args) {
+    ArrayList<String> arrayString = new ArrayList<>();
+    arrayString.add("Diet");
+    arrayString.add("Value1");
+    arrayString.add("Value2");
+    arrayString.add("Value3");
+
+    System.out.println(arrayString);
+
+  }
+}
